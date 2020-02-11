50bd9e964be10fbdd7f4185c6c489e463f435529
Amanda Lopes <amanda.lcosta33@gmail.com>
2020-02-10 22:31:48 -0300

    Comeco do site boostore

---
+++ AULA9/funcoes.js
@@ -0,0 +1,16 @@
+function Dia(){
+    alert("Bom dia");
+}
+
+function Tarde(){
+    alert("Boa tarde");
+}
+
+function Noite(){
+    alert("Boa noite");
+}
+
+function Ola(texto, numero){
+    alert(texto);
+    console.log(numero);
+}
---
+++ JS boostore/function.js
@@ -0,0 +1,7 @@
+function Cadastro(){
+    let cadastroEmail = document.querySelector("#email").value;
+    console.log(cadastroEmail);
+
+    let cadastroSenha = document.querySelector("#senha").value;
+    console.log(cadastroSenha);
+}
